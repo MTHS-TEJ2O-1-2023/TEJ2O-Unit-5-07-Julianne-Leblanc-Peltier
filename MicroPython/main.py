ae71a0f09d44442740cc33623903d64e4f2ca998
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-11-08 18:05:59 +0000

    python code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,29 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import robotbit
 
+# variables
+servo_number_1 = robotbit.servo(1, 0)
 
-display.scroll("Hello, World!")
+# set up
+display.show(Image.HAPPY)
+
+# when button a is pressed, turns servo to 0 degrees.
+while True:
+    if button_a.is_pressed():
+        robotbit.servo(1, 0)
+        display.clear()
+        print("0")
+        display.show(Image.SQUARE_SMALL)
+        display.clear()
+        display.show(Image.HAPPY)
+
+    # when button b is pressed, turns servo to 180 degrees.
+    if button_b.is_pressed():
+        robotbit.servo(1, 180)
+        display.clear()
+        print("180")
+        display.show(Image.SQUARE_SMALL)
+        display.clear()
+        display.show(Image.HAPPY)
