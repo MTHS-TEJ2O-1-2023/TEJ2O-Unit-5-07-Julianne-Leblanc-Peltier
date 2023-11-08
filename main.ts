f39c367da5456cdfc799538a425103acaf2725f7
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-11-08 12:54:38 -0500

    indent fix

--- main.ts
+++ main.ts
@@ -13,20 +13,20 @@ basic.showIcon(IconNames.Happy)
 
 // when button a is pressed, turns servo to 0 degrees.
 input.onButtonPressed(Button.A, function () {
-    robotbit.Servo(servoNumber1, 0)
-    basic.clearScreen()
-    basic.showString('0')
-    basic.showIcon(IconNames.SmallSquare)
-    basic.clearScreen()
-    basic.showIcon(IconNames.Happy)
+  robotbit.Servo(servoNumber1, 0)
+  basic.clearScreen()
+  basic.showString('0')
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
 })
 
 // when button b is pressed, turns servo to 180 degrees.
 input.onButtonPressed(Button.B, function () {
-    robotbit.Servo(servoNumber1, 180)
-    basic.clearScreen()
-    basic.showString('180')
-    basic.showIcon(IconNames.SmallSquare)
-    basic.clearScreen()
-    basic.showIcon(IconNames.Happy)
+  robotbit.Servo(servoNumber1, 180)
+  basic.clearScreen()
+  basic.showString('180')
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
 })
