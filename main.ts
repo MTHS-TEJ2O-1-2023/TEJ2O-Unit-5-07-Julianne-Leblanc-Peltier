df08618efb789089b7b0766c3a3718fee506434a
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-11-08 12:50:12 -0500

    typescript code

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Julianne Leblanc-Peltier
+ * Created on: Oct 2023
+ * This program makes a servo turn through a MicroBit in typescript
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+// when button a is pressed, turns servo to 0 degrees.
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 0)
+    basic.clearScreen()
+    basic.showString('0')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+// when button b is pressed, turns servo to 180 degrees.
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString('180')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
